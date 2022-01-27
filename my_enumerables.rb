537f1d6d0c6b5706c8cdc687b4b40c180c5c1e9a
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2022-01-27 16:30:08 +0200

    Fix error

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -1,23 +1,17 @@
-module MyEnumerable 
+module MyEnumerable
+  def all?
+    each { |num| return false unless yield num }
+    true
+  end
 
-  def all? each { |num| 
-    return false unless yield num 
-  }
-     true 
-  end 
-  
-  def any? each { |num| 
-    return true 
-    if yield num 
-    } 
-    false 
-  end 
-  
-  def filter 
-    arr = [] 
-    each { |num| 
-      arr.push(num) if yield num 
-    } 
-    arr 
-  end 
-end 
+  def any?
+    each { |num| return true if yield num }
+    false
+  end
+
+  def filter
+    arr = []
+    each { |num| arr.push(num) if yield num }
+    arr
+  end
+end
