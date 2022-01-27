3b885717b65ff48154809151809c2d03c557ae7e
shoirata <shoira.shakirovna@gmail.com>
2022-01-27 18:30:45 +0400

    Fix bug

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -1,23 +1,33 @@
-module MyEnumerable 
+module MyEnumerable
+  def all?
+    @list.each do |num|
+      return true unless block_given?
 
-  def all? each { |num| 
-    return false unless yield num 
-  }
-     true 
-  end 
-  
-  def any? each { |num| 
-    return true 
-    if yield num 
-    } 
-    false 
-  end 
-  
-  def filter 
-    arr = [] 
-    each { |num| 
-      arr.push(num) if yield num 
-    } 
-    arr 
-  end 
-end 
+      block_test = yield(num)
+      return false unless block_test
+    end
+    true
+  end
+
+  def any?
+    @list.each do |num|
+      return true unless block_given?
+
+      block_test = yield(num)
+      return true if block_test
+    end
+    false
+  end
+
+  def filter
+    return @list unless block_given?
+    return @list if @list.length.zero?
+
+    result = []
+    @list.each do |num|
+      block_test = yield(num)
+      result.push(num) if block_test
+    end
+    result
+  end
+end
